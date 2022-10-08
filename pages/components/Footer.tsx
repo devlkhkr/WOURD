6c9e1944ea04f859965c1c53e34285944fb52256
bibiboy <95161113+chromeheartz@users.noreply.github.com>
2022-10-08 17:29:54 +0900

    [Modify] scss 구조 변경

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -1,7 +1,33 @@
 import React from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPoo } from "@fortawesome/free-solid-svg-icons";
+
 const Footer: React.FC = () => {
-  return <footer></footer>;
+  return (
+    <footer>
+      <div className="inner">
+        <ul>
+          <li>
+            <FontAwesomeIcon icon={faPoo} />
+            <span>메인</span>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faPoo} />
+            <span>단어장</span>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faPoo} />
+            <span>단어관리</span>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faPoo} />
+            <span>설정</span>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBolt } from "@fortawesome/free-solid-svg-icons";
+
+import styles from "../../styles/scss/import/_common.module.scss";
+
 const Header: React.FC = () => {
-  return <header></header>;
+  return (
+    <header>
+      {/* <FontAwesomeIcon icon="fa-brands fa-discord" /> */}
+      <div className="header-logo">
+        <FontAwesomeIcon icon={faBolt} />
+        <h1 className={styles.tit_header_logo}>hello</h1>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
