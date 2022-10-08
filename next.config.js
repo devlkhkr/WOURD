6ceb88361452240361c973fd59323e0bcd0a363f
LeeKangHyoung <devlkhkr@gmail.com>
2022-10-08 14:31:01 +0900

    atomic design arc

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles/scss/pages')],
+  },  
 }
 
 module.exports = nextConfig
