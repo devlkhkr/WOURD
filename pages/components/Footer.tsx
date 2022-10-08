27b94f3467564905599edcbea09c70f8e2ba5e81
bibiboy <95161113+chromeheartz@users.noreply.github.com>
2022-10-08 20:14:02 +0900

    [Modify] header, footer 컴포넌트화

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -1,32 +1,97 @@
 import React from "react";
+import Link from "next/link";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPoo } from "@fortawesome/free-solid-svg-icons";
+import {
+  faHouse,
+  faNoteSticky,
+  faNotesMedical,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
+
+import styled from "styled-components";
+const FooterWrap = styled.footer`
+  ul {
+    display: flex;
+    align-items: center;
+    border-top: 1px solid var(--color-grey);
+    li {
+      border-right: 1px solid var(--color-grey);
+      background-color: var(--color-white);
+      width: 25%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      padding: 8px 0;
+      &:first-child {
+        border-left: 1px solid var(--color-grey);
+      }
+    }
+  }
+`;
+
+const ButtonIcon = styled.i`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 30px;
+  height: 30px;
+  margin-bottom: 4px;
+  svg {
+    font-size: 20px;
+  }
+`;
+
+const ButtonText = styled.span`
+  font-weight: var(--weight-medium);
+  color: var(--color-black);
+  font-size: 16px;
+`;
 
 const Footer: React.FC = () => {
   return (
-    <footer>
-      <div className="inner">
-        <ul>
-          <li>
-            <FontAwesomeIcon icon={faPoo} />
-            <span>메인</span>
-          </li>
-          <li>
-            <FontAwesomeIcon icon={faPoo} />
-            <span>단어장</span>
-          </li>
-          <li>
-            <FontAwesomeIcon icon={faPoo} />
-            <span>단어관리</span>
-          </li>
-          <li>
-            <FontAwesomeIcon icon={faPoo} />
-            <span>설정</span>
-          </li>
-        </ul>
-      </div>
-    </footer>
+    <FooterWrap>
+      <ul>
+        <li>
+          <Link href="/RegistWord">
+            <a>Home</a>
+          </Link>
+        </li>
+        {/* <li>
+          <Link href="/">
+            <ButtonIcon>
+              <FontAwesomeIcon icon={faHouse} />
+            </ButtonIcon>
+            <ButtonText>메인</ButtonText>
+          </Link>
+        </li> */}
+        {/* <li>
+          <Link href="/RegistWord">
+            <ButtonIcon>
+              <FontAwesomeIcon icon={faNoteSticky} />
+            </ButtonIcon>
+            <ButtonText>단어장</ButtonText>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <ButtonIcon>
+              <FontAwesomeIcon icon={faNotesMedical} />
+            </ButtonIcon>
+            <ButtonText>단어관리</ButtonText>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <ButtonIcon>
+              <FontAwesomeIcon icon={faInfoCircle} />
+            </ButtonIcon>
+            <ButtonText>설정</ButtonText>
+          </Link>
+        </li> */}
+      </ul>
+    </FooterWrap>
   );
 };
 
--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -3,17 +3,41 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBolt } from "@fortawesome/free-solid-svg-icons";
 
-import styles from "../../styles/scss/import/_common.module.scss";
+import styled from "styled-components";
+
+const HeaderWrap = styled.header`
+  background-color: blue;
+  width: 100%;
+  height: 50px;
+  background-color: #ffffff;
+  position: relative;
+  display: flex;
+  justify-content: center;
+`;
+
+const HeaderLogo = styled.div`
+  display: flex;
+  align-items: center;
+  svg {
+    font-size: 26px;
+    color: #0047ab;
+  }
+  h1 {
+    font-size: 26px;
+    color: #0047ab;
+    font-weight: var(--weight-black);
+  }
+`;
 
 const Header: React.FC = () => {
   return (
-    <header>
+    <HeaderWrap>
       {/* <FontAwesomeIcon icon="fa-brands fa-discord" /> */}
-      <div className="header-logo">
+      <HeaderLogo>
         <FontAwesomeIcon icon={faBolt} />
-        <h1 className={styles.tit_header_logo}>hello</h1>
-      </div>
-    </header>
+        <h1>DINDER</h1>
+      </HeaderLogo>
+    </HeaderWrap>
   );
 };
 
