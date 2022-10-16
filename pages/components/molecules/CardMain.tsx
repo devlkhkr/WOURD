3146e79e6dd1dcf8b7d815a9275fe959c2eccf80
LeeKangHyoung <84236655+devlkhkr@users.noreply.github.com>
2022-10-17 05:49:53 +0900

    [Modify] 메인 카드 스와이프 수정

--- pages/components/molecules/CardMain.tsx
+++ pages/components/molecules/CardMain.tsx
@@ -2,76 +2,168 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import Button from "../../components/atoms/Button";
 import Typo from "../../components/atoms/Typo";
+import { CardSwiper } from "react-card-rotate-swiper";
+import { dir } from "console";
 
 interface CardMainTypes {
   exposeWord: ExposeWordTypes[];
 }
-
 interface ExposeWordTypes {
   word: string;
   unravel?: string;
   desc: string;
+  fliped?: boolean;
 }
+
+const CardBaseStyle = `
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-flow: column;
+  width: 100%;
+  height: 100%;
+  padding: 16px;
+  position: absolute;
+  text-align: center;
+  backface-visibility: hidden;
+`;
+
+const MainWrapStyled = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  .card {
+    width: 100%;
+    height: calc(100% - 80px);
+    position: absolute;
+    &.fliped .cardMain {
+      transform: rotateY(180deg);
+      transition: transform 0.5s;
+    }
+    &.fliped ~ .btn_wrap_cardctrl {
+      opacity: 1;
+      pointer-event: all;
+    }
+  }
+`;
 
 const CardWrapStyled = styled.div`
   width: 100%;
   height: 100%;
-  background-color: #fff;
-  border-radius: 8px;
-  padding: 16px;
+  position: relative;
 `;
 
 const CardMainStyled = styled.div<CardMainTypes>`
-  height: calc(100% - 40px);
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  border-radius: 8px;
+
+  position: absolute;
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.1);
+
+  transition: transform 0.5s;
+  transform-style: preserve-3d;
+`;
+
+const CardFrontStyled = styled.div`
+  ${CardBaseStyle}
+`;
+
+const CardBackStyled = styled.div`
+  ${CardBaseStyle}
+  transform: rotateY(180deg);
 `;
 
 const BtnWrapCardCtrlStyled = styled.div`
-  height: 40px;
-  text-align: center;
+  position: absolute;
+  bottom: 0;
+  opacity: 0;
+  pointer-event: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 80px;
+  transition-duration: 0.5s;
   button + button {
     margin-left: 8px;
   }
 `;
 
-const InputRadioComponent: React.FC<CardMainTypes> = ({
-  exposeWord
-}) => {
+const CardMainComponent: React.FC<CardMainTypes> = ({ exposeWord }) => {
   const [wordIdx, setWordIdx] = useState(0);
   const cardPrevClick = function () {
+    setFlagFlip(false);
     if (wordIdx != 0) setWordIdx(wordIdx - 1);
-  }
+  };
   const cardNextClick = function () {
-    if (wordIdx < (exposeWord.length - 1)) setWordIdx(wordIdx + 1);
-  }
+    setFlagFlip(false);
+    if (wordIdx < exposeWord.length - 1) setWordIdx(wordIdx + 1);
+  };
+
+  const [flagFlip, setFlagFlip] = useState(false);
+  const [wordList, setWordList] = useState([...exposeWord]);
   return (
-    <CardWrapStyled>
-      <CardMainStyled
-        exposeWord={exposeWord}
-      >
-        <Typo type="typo-lg">{exposeWord[wordIdx].word}</Typo>
-        <Typo>{exposeWord[wordIdx].unravel}</Typo>
-        <Typo>{exposeWord[wordIdx].desc}</Typo>
-      </CardMainStyled>
-      <BtnWrapCardCtrlStyled>
+    <MainWrapStyled>
+      {wordList.map((objWord, index) => (
+        <CardSwiper
+          key={index}
+          onSwipe={function (d: string) {
+            switch (d) {
+              case "none":
+                objWord.fliped = true;
+                setWordList([...wordList]);
+                break;
+              case "left":
+              case "right":
+                setTimeout(function () {
+                  objWord.fliped = false;
+                  setWordList([...wordList]);
+                }, 700);
+            }
+          }}
+          className={`card ${objWord.fliped ? "fliped" : ""}`}
+          throwLimit={360}
+          contents={
+            <CardWrapStyled>
+              <CardMainStyled exposeWord={exposeWord} className="cardMain">
+                <CardFrontStyled>
+                  <div>{objWord.fliped}</div>
+                  <Typo type="typo-lg">{objWord.word}</Typo>
+                </CardFrontStyled>
+                <CardBackStyled>
+                  <Typo type="typo-lg">{objWord.word}</Typo>
+                  <Typo>{objWord.unravel}</Typo>
+                  <Typo>{objWord.desc}</Typo>
+                </CardBackStyled>
+              </CardMainStyled>
+            </CardWrapStyled>
+          }
+        />
+      ))}
+      <BtnWrapCardCtrlStyled className="btn_wrap_cardctrl">
         <Button
           desc="이전카드"
           bgc="#666"
           color="#fff"
           width="40%"
           height="40px"
-          onClick={cardPrevClick} />
+          onClick={cardPrevClick}
+        />
         <Button
           desc="다음카드"
           bgc="#666"
           color="#fff"
           width="40%"
           height="40px"
-          onClick={cardNextClick} />
+          onClick={cardNextClick}
+        />
       </BtnWrapCardCtrlStyled>
-    </CardWrapStyled>
+    </MainWrapStyled>
   );
 };
 
-InputRadioComponent.defaultProps = {};
+CardMainComponent.defaultProps = {};
 
-export default InputRadioComponent;
+export default CardMainComponent;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,26 @@
 import type { NextPage } from "next";
 import { useState } from "react";
-import CardMain from "./components/molecules/CardMain"
-
+import CardMain from "./components/molecules/CardMain";
 
 const Home: NextPage = () => {
   const [exposeWord, setExposeWord] = useState([
     {
       word: "SSR",
       unravel: "Server Side Rendering",
-      desc: "SSR이란 서버사이드 렌더링(Server Side Rendering)의 약자로 서버로부터 완전하게 만들어진 HTML 파일을 받아와 페이지 전체를 렌더링 하는 방식이다."
+      desc: "SSR이란 서버사이드 렌더링(Server Side Rendering)의 약자로 서버로부터 완전하게 만들어진 HTML 파일을 받아와 페이지 전체를 렌더링 하는 방식이다.",
+      fliped: false,
     },
     {
       word: "Maturity",
       desc: "(서비스) 성숙도, 완성도",
+      fliped: false,
     },
     {
       word: "Framework",
       desc: "프레임워크(Framework)란, 소프트웨어의 구체적인 부분에 해당하는 설계와 구현을 재사용이 가능하게끔 일련의 협업화된 형태로 클래스들을 제공하는 것",
-    }
-
-  ])
+      fliped: false,
+    },
+  ]);
   return (
     <>
       <CardMain exposeWord={exposeWord} />
