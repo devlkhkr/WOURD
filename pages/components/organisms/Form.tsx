a160826b8f96d83fd481b439485cb251ca678d56
LeeKangHyoung <devlkhkr@gmail.com>
2022-10-19 18:31:30 +0900

    [Modify] 폼 컴포넌트 오탈자 수정

--- pages/components/organisms/Form.tsx
+++ pages/components/organisms/Form.tsx
@@ -4,14 +4,14 @@ interface FormTypes {
   children: any;
 }
 
-const FormtSyled = styled.form<FormTypes>`
+const FormtStyled = styled.form<FormTypes>`
   padding: 16px;
   background-color: #fff;
   border-radius: 8px;
 `;
 
 const FormComponent: React.FC<FormTypes> = ({ children }) => {
-  return <FormtSyled>{children}</FormtSyled>;
+  return <FormtStyled>{children}</FormtStyled>;
 };
 
 FormComponent.defaultProps = {};
