33204acf09b8162efe626ff6ddc998b315730843
bibiboy <95161113+chromeheartz@users.noreply.github.com>
2022-10-19 23:20:50 +0900

    [Modify] 세팅 리스트 디자인 수정

--- pages/Setting/index.tsx
+++ pages/Setting/index.tsx
@@ -1,33 +1,65 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
 import styled from "styled-components";
 import SettingListComponent from "../components/molecules/SettingList";
 
 interface SettingTypes {
-  typo: String;
+  typo: string;
+  nextStep?: boolean;
+  rightText?: string;
 }
+
+const SettingWrap = styled.div`
+  background-color : var(--color-white);
+  border-radius : 16px;
+  padding : 16px 16px 80px;
+  display : flex;
+  flex-direction : column;
+  height : 100%;
+`
 
 // user interface
 const SettingUserStyled = styled.div`
-  
-`
+  margin-bottom : 2rem;
+`;
 
 // app interface
-const SettingBottomStyled = styled.div`
-  
-`;
+const SettingBottomStyled = styled.div``;
 
 const Setting: NextPage<SettingTypes> = () => {
   return (
-    <>
-      <div>유저상단영역</div>
+    <SettingWrap>
+      <div>유저프로필영역</div>
       <SettingUserStyled>
-        <SettingListComponent typo="비밀번호 변경" />
+        <SettingListComponent 
+          typo="비밀번호 변경"
+          nextStep={true}
+        />
+        <SettingListComponent 
+          typo="공지사항"
+          nextStep={true}
+        />
+        <SettingListComponent 
+          typo="탈퇴요청"
+          nextStep={true}
+        />
       </SettingUserStyled>
-      
+
       <SettingBottomStyled>
-        <SettingListComponent typo="도움말" />
+        <SettingListComponent 
+          typo="도움말(FAQ)"
+          nextStep={true}
+        />
+        <SettingListComponent 
+          typo="로그아웃"
+          nextStep={true}
+        />
+        <SettingListComponent 
+          typo="버전정보" 
+          rightText="1.0.0"
+        />
       </SettingBottomStyled>
-    </>
+    </SettingWrap>
   );
 };
 
--- pages/components/molecules/SettingList.tsx
+++ pages/components/molecules/SettingList.tsx
@@ -6,24 +6,42 @@ import TypoComponent from "../atoms/Typo";
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 interface SettingListTypes {
-  typo: String;
+  typo: string;
+  nextStep ?: boolean;
+  rightText ?: string;
 }
 
 const SettingList = styled.div`
   display : flex;
   align-items : center;
   justify-content : space-between;
-  color : $black;
+  color : var(--color-black);
+  padding : 8px 0;
+  border-bottom : 1px solid var(--color-deepgrey);
+  &:first-child {
+    border-top : 1px solid var(--color-deepgrey);
+  }
 `
 
 const SettingListComponent: React.FC<SettingListTypes> = props => {
-  const { typo } = props;
+  const { typo, nextStep, rightText } = props;
   return (
     <SettingList>
-      <TypoComponent weight="regular" align="left">
+      <TypoComponent size="14px" weight="regular" align="left">
         {typo}
       </TypoComponent>
-      <Icon iconShape={faChevronRight} iconWidth={24} iconHeight={24} bottom={4} />
+      {
+        nextStep ? (
+          // next step
+          <Icon iconShape={faChevronRight} iconWidth={16} iconHeight={16} bottom={4} />
+        ) : (
+          // version text
+          <TypoComponent size="14px" weight="semi-bold">
+            {rightText}
+          </TypoComponent>
+        )
+        
+      }
     </SettingList>
   );
 };
