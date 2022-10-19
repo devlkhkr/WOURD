613d370b61b471812c44ec2b113de50ae96cc431
LeeKangHyoung <devlkhkr@gmail.com>
2022-10-19 20:44:51 +0900

    [Modify] 회원가입 폼 구조 변경

--- pages/components/templates/Join.tsx
+++ pages/components/templates/Join.tsx
@@ -6,7 +6,6 @@ import Radio from "../../components/atoms/Radio";
 import Select from "../../components/atoms/Select";
 import MultiSelect from "../../components/atoms/MultiSelect";
 import Label from "../../components/atoms/Label";
-import Textarea from "../../components/atoms/Textarea";
 import Button from "../../components/atoms/Button";
 import Fieldset from "../../components/molecules/Fieldset";
 import InputWrap from "../../components/molecules/InputWrap";
@@ -29,12 +28,6 @@ const JoinStyled = styled.form<LoginTypes>`
   padding: 32px 16px;
   background-color: #f3f3f3;
   overflow-y: auto;
-  input{
-    margin-top: 8px;
-  }
-  button{
-    margin-top: 16px;
-  }
 `;
 
 const JoinComponent: React.FC<LoginTypes> = ({ setJoinPageOpened }) => {
@@ -48,29 +41,49 @@ const JoinComponent: React.FC<LoginTypes> = ({ setJoinPageOpened }) => {
         <Form>
           <Typo size="16px" weight="semi-bold" mt="12px">회원가입</Typo>
           <Fieldset>
-            <Label
-              htmlFor="joinId"
-              desc="아이디"
-              mandatory={true}
-            />
-            <InputText type="text" placeHolder="사용하실 아이디를 입력해주세요." id="joinId" />
-          </Fieldset>
-          <Fieldset>
-            <Label
-              htmlFor="joinPw"
-              desc="비밀번호"
-              mandatory={true}
-            />
-            <InputText type="password" placeHolder="최소 8자리 이상, 영문자 + 숫자 조합" id="joinPw" />
+            <InputWrap>
+              <Label
+                htmlFor="joinId"
+                desc="아이디"
+                mandatory={true}
+              />
+              <InputText type="text" placeHolder="사용하실 아이디를 입력해주세요." id="joinId" />
+            </InputWrap>
+            <InputWrap>
+              <Label
+                htmlFor="joinPw"
+                desc="비밀번호"
+                mandatory={true}
+              />
+              <InputText type="password" placeHolder="최소 8자리 이상, 영문자 + 숫자 조합" id="joinPw" />
+            </InputWrap>
+            <InputWrap>
+              <Label
+                htmlFor="joinPwConfirm"
+                desc="비밀번호 확인"
+                mandatory={true}
+              />
+              <InputText type="password" placeHolder="비밀번호를 한번 더 입력해주세요." id="joinPwConfirm" />
+            </InputWrap>
           </Fieldset>
 
           <Fieldset>
-            <Label
-              htmlFor="joinKey"
-              desc="초대코드"
-              mandatory={true}
-            />
-            <InputText type="text" placeHolder="초대코드를 입력해주세요." id="joinKey" />
+            <InputWrap>
+              <Label
+                htmlFor="joinName"
+                desc="성함"
+                mandatory={true}
+              />
+              <InputText type="text" placeHolder="실명을 입력해주세요." id="joinName" />
+            </InputWrap>
+            <InputWrap>
+              <Label
+                htmlFor="joinKey"
+                desc="초대코드"
+                mandatory={true}
+              />
+              <InputText type="text" placeHolder="초대코드를 입력해주세요." id="joinKey" />
+            </InputWrap>
           </Fieldset>
 
           <Fieldset>
