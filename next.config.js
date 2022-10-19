6ce532fc876990bcfc9b6c4574ee3b2b15b5b396
LeeKangHyoung <devlkhkr@gmail.com>
2022-10-19 15:47:33 +0900

    [Docs] NextJS 기본 로딩 UI 제거

--- next.config.js
+++ next.config.js
@@ -4,9 +4,12 @@ const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  devIndicators: {
+    buildActivity: false //nexjs 기본 로딩 UI
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles/scss/pages')],
-  },  
+  },
 }
 
 module.exports = nextConfig
