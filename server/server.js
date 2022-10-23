870fcec1c027876ac2c6178f4bd032658315e8ae
LeeKangHyoung <devlkhkr@gmail.com>
2022-10-23 19:41:14 +0900

    [Feat] API 서버 생성

---
+++ server/server.js
@@ -0,0 +1,123 @@
+// S : sql setting
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 9090;
+const db = require('./config/db');
+const bodyParser = require('body-parser');
+// const path = require("path");
+// const cors = require('cors');
+
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(bodyParser.json());
+// app.use(cors({
+//     origin: 'http://saulter.co.kr',
+//     credentials: true,
+// }));
+
+// app.use(express.static(path.join(__dirname, "build")));
+
+// app.get("/", (req, res) => {
+//     res.set({
+//         "Cache-Control": "no-cache, no-store, must-revalidate",
+//         Pragma: "no-cache",
+//         Date: Date.now()
+//     });
+//     res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
+
+app.get('/api/host', (req, res) => {
+    res.send({
+        host: 'kany'
+    });
+})
+
+app.listen(PORT, () => {
+    console.log(`Server On : http://localhost:${PORT}/`);
+})
+// E : sql setting
+
+app.post('/api/board/fav', (req, res) => {
+    db.query("UPDATE BOARD_TB SET BOARD_FAV=" + req.body.fav + " WHERE BOARD_SEQ=" + +req.body.seq, (err, data) => {
+        if (!err) {
+            res.send(data);
+        } else {
+            console.log(err);
+            res.send(err);
+        }
+    })
+});
+
+
+app.post('/api/board/del', (req, res) => {
+    db.query("DELETE FROM BOARD_TB WHERE BOARD_SEQ=" + +req.body.seq, (err, data) => {
+        if (!err) {
+            res.send(data);
+
+        } else {
+            console.log(err);
+            res.send(err);
+        }
+    })
+});
+
+app.get('/api/board/list', (req, res) => {
+    db.query("SELECT * FROM BOARD_TB", (err, data) => {
+        if (!err) {
+            res.send(data);
+
+        } else {
+            console.log(err);
+            res.send(err);
+        }
+    })
+});
+
+app.get('/api/devlog/list', (req, res) => {
+    db.query("SELECT * FROM DEVLOG_TB", (err, data) => {
+        if (!err) {
+            res.send(data);
+
+        } else {
+            console.log(err);
+            res.send(err);
+        }
+    })
+});
+
+app.get('/api/project/list', (req, res) => {
+    db.query("SELECT * FROM PROJECT_TB", (err, data) => {
+        if (!err) {
+            res.send(data);
+
+        } else {
+            console.log(err);
+            res.send(err);
+        }
+    })
+});
+
+app.get('/api/s3/info', (req, res) => {
+    db.query("SELECT * FROM S3_INFO_TB", (err, data) => {
+        if (!err) {
+            res.send(data);
+
+        } else {
+            console.log(err);
+            res.send(err);
+        }
+    })
+});
+
+app.post('/api/devlog/write', (req, res) => {
+    db.query(`INSERT INTO DEVLOG_TB (DEVLOG_SEQ, DEVLOG_TITLE, DEVLOG_DETAIL, DEVLOG_IMG_URL, DEVLOG_WRITED_TIME, DEVLOG_CATEGORY, DEVLOG_WRITER) VALUES (null, "${req.body.postTitle}", '${req.body.postDetail}', "${req.body.postImgUrl}", NOW(), "REACT", "Lee Kanghyoung" )`
+        , (err, data) => {
+        if (!err) {
+            res.send(data);
+        } else {
+            console.log(err);
+            res.send(err);
+        }
+    })
+});
