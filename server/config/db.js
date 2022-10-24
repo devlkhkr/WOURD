214dbc55ffec051ee2aa072aeb43f8f481b8acfb
LeeKangHyoung <devlkhkr@gmail.com>
2022-10-24 10:37:00 +0900

    [Test] git ignore 관리 테스트

--- server/config/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql');
-const db = mysql.createPool({
-  host     : 'copubdb.cf6qeul68kjk.ap-northeast-1.rds.amazonaws.com',
-  user     : 'admin',
-  password : '',
-  database : 'COPUBDB',
-  charset : 'utf8mb4'
-});
-module.exports = db;
