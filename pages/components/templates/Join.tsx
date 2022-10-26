721654669e0aea9914c3ae821a6d4f2029dbc0c3
LeeKangHyoung <devlkhkr@gmail.com>
2022-10-26 16:57:26 +0900

    [Modify] 회원가입 이메일 length check

--- pages/components/templates/Join.tsx
+++ pages/components/templates/Join.tsx
@@ -127,6 +127,9 @@ const JoinComponent: React.FC<LoginTypes> = ({ setJoinPageOpened }) => {
         if(authCheckFlag){
       
         }
+        else if(joinUserId.length === 0) {
+          alert("이메일을 입력해주세요.")
+        }
         else if(!validator.isEmail(joinUserId)){
           alert("유효한 이메일 형식이 아닙니다.")
         }
