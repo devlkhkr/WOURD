377256ef25f456c856f464224b37cf07bd880547
bibiboy <95161113+chromeheartz@users.noreply.github.com>
2022-10-26 23:28:10 +0900

    [Modify] 버젼정보 텍스트 props, type 수정

--- pages/Setting/index.tsx
+++ pages/Setting/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import Link from "next/link";
+import { useState } from "react";
 import styled from "styled-components";
 import styledInterface from "../components/Intefaces/styledComponent";
 import SettingListComponent from "../components/molecules/SettingList";
@@ -8,7 +8,7 @@ import UserProfileComponent from "../components/molecules/UserProfile";
 interface SettingTypes extends styledInterface {
   typo: string;
   afterIcon?: string;
-  rightText?: string;
+  rightTypo?: string;
 }
 
 const SettingWrap = styled.div`
@@ -37,6 +37,11 @@ const SettingTopStyled = styled.div`
 const SettingBottomStyled = styled.div``;
 
 const Setting: NextPage<SettingTypes> = () => {
+  const [wordControl, setwordControl] = useState(false);
+  
+  console.log(wordControl)
+
+
   return (
     <SettingWrap>
       <SettingProfileStyled>
@@ -44,21 +49,30 @@ const Setting: NextPage<SettingTypes> = () => {
       </SettingProfileStyled>
 
       <SettingTopStyled>
-        <SettingListComponent typo="단어노출제어" afterIcon='arr-down' />
+        <SettingListComponent 
+          typo="단어노출제어" 
+          afterIcon="arr-down"
+          onClick={() => {
+            setwordControl(prev => !prev);
+          }}
+        />
         {/* 
           아는단어
           모르는단어
           즐겨찾는단어
           건너뛴단어
          */}
+        {
+          // 아코디언
+          wordControl && <div>hello</div>
+        }
       </SettingTopStyled>
 
-      
       <SettingBottomStyled>
         <SettingListComponent typo="공지사항" />
         <SettingListComponent typo="도움말(FAQ)" />
         <SettingListComponent typo="개발히스토리" />
-        <SettingListComponent typo="버전정보" rightText="1.0.0" />
+        <SettingListComponent typo="버전정보" rightTypo="1.0.0" />
         <SettingListComponent typo="시스템스펙" />
         <SettingListComponent typo="로그아웃" />
       </SettingBottomStyled>
--- pages/components/molecules/SettingList.tsx
+++ pages/components/molecules/SettingList.tsx
@@ -4,50 +4,74 @@ import styledInterface from "../Intefaces/styledComponent";
 import Icon from "../atoms/Icon";
 import TypoComponent from "../atoms/Typo";
 
-import { faChevronRight, faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import {
+  faChevronRight,
+  faChevronDown,
+  faChevronUp,
+} from "@fortawesome/free-solid-svg-icons";
 
-interface SettingListTypes extends styledInterface{
+interface SettingListTypes extends styledInterface {
   typo: string;
-  afterIcon ?: string;
-  rightText ?: string;
+  afterIcon?: string;
+  rightTypo?: string;
 }
 
 const SettingList = styled.div`
-  height : 40px;
-  display : flex;
-  align-items : center;
-  justify-content : space-between;
-  color : var(--color-black);
-  padding : 0 8px;
-  border-bottom : 1px dashed rgba(120,120,120, 0.2);
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: var(--color-black);
+  padding: 0 8px;
+  border-bottom: 1px dashed rgba(120, 120, 120, 0.2);
   &:first-child {
-    border-top : 1px solid rgba(120,120,120, 0.45);
+    border-top: 1px solid rgba(120, 120, 120, 0.45);
   }
-`
+`;
 
 const SettingListComponent: React.FC<SettingListTypes> = props => {
-  const { typo, afterIcon, rightText } = props;
+  const { typo, afterIcon, rightTypo, onClick } = props;
   const setAfterIcon = () => {
-    switch(afterIcon){
-      case 'arr-right':
-        return <Icon iconShape={faChevronRight} iconWidth="16px" iconHeight="16px" svgSize="12px"/>
-      case 'arr-down':
-        return <Icon iconShape={faChevronDown} iconWidth="16px" iconHeight="16px" svgSize="12px" />
-      case 'arr-up':
-        return <Icon iconShape={faChevronUp} iconWidth="16px" iconHeight="16px" svgSize="12px" />
+    switch (afterIcon) {
+      case "arr-right":
+        return (
+          <Icon
+            iconShape={faChevronRight}
+            iconWidth="16px"
+            iconHeight="16px"
+            svgSize="12px"
+          />
+        );
+      case "arr-down":
+        return (
+          <Icon
+            iconShape={faChevronDown}
+            iconWidth="16px"
+            iconHeight="16px"
+            svgSize="12px"
+          />
+        );
+      case "arr-up":
+        return (
+          <Icon
+            iconShape={faChevronUp}
+            iconWidth="16px"
+            iconHeight="16px"
+            svgSize="12px"
+          />
+        );
       default:
-        return 
+        return;
     }
-  }
-  
+  };
+
   return (
-    <SettingList>
+    <SettingList onClick={onClick}>
       <TypoComponent fontSize="14px" fontWeight="regular" textAlign="left">
         {typo}
       </TypoComponent>
-      {
-        setAfterIcon()
-      }
+      {rightTypo && <span>{rightTypo}</span>}
+      {afterIcon && setAfterIcon()}
     </SettingList>
   );
 };
