a1c8390f4842d842b1fede44a77facbf01882ca8
bibiboy <95161113+chromeheartz@users.noreply.github.com>
2022-10-29 15:01:08 +0900

    [Add] userProfile쪽 ui구현전 페이지 만들기

---
+++ pages/Setting/UserProfile.tsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+import styled from "styled-components";
+import styledInterface from "../components/Intefaces/styledComponent";
+
+const UserProfile = () => {
+
+  return (
+    <>
+      <div>유저맨</div>
+    </>
+  );
+};
+
+export default UserProfile;
