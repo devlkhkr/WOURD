6792036500c72611347a8635241f5b1c88afc535
LeeKangHyoung <84236655+devlkhkr@users.noreply.github.com>
2022-11-01 14:58:05 +0900

    [Modify] Form 컴포넌트 오탈자 수정

--- pages/components/organisms/Form.tsx
+++ pages/components/organisms/Form.tsx
@@ -1,19 +1,27 @@
 import React from "react";
 import styled from "styled-components";
-import styledInterface from "../Intefaces/styledComponent"
-interface FormTypes extends styledInterface{
+import styledInterface from "../Intefaces/styledComponent";
+interface FormTypes extends styledInterface {
   children: any;
   onSubmit?: React.FormEventHandler<HTMLFormElement>;
 }
 
-const FormtStyled = styled.form<FormTypes>`
+const FormStyled = styled.form<FormTypes>`
   padding: 16px;
   background-color: #fff;
   border-radius: 8px;
 `;
 
-const FormComponent: React.FC<FormTypes> = ({ children, onSubmit, reference }) => {
-  return <FormtStyled onSubmit={onSubmit} ref={reference}>{children}</FormtStyled>;
+const FormComponent: React.FC<FormTypes> = ({
+  children,
+  onSubmit,
+  reference,
+}) => {
+  return (
+    <FormStyled onSubmit={onSubmit} ref={reference}>
+      {children}
+    </FormStyled>
+  );
 };
 
 FormComponent.defaultProps = {};
