c6bef690d1685ebdaea2619dee4e6bfff82e543d
bibiboy <95161113+chromeheartz@users.noreply.github.com>
2022-11-21 23:48:09 +0900

    [Modify] github, insta, mail 아이콘 추가

--- pages/components/molecules/SystemDevs.tsx
+++ pages/components/molecules/SystemDevs.tsx
@@ -1,9 +1,11 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import styled from "styled-components";
 import ImgComponent from "../atoms/Img";
 import TypoComponent from "../atoms/Typo";
 import styledInterface from "../Intefaces/styledComponent";
+import Icon from "../atoms/Icon";
+import { faGithub, faInstagramSquare } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 interface SystemDevType extends styledInterface {
   pos: string;
   name: string;
@@ -12,35 +14,39 @@ interface SystemDevType extends styledInterface {
 }
 
 const DevsItem = styled.li`
-  width : 50%;
-  border-radius : 16px;
+  width: 50%;
+  border-radius: 16px;
   box-shadow: 0px 2px 4px 2px rgba(0, 0, 0, 0.1);
-  & > div {
-    display : flex;
-    flex-direction : column;
-    justify-content : center;
-    align-items : center;
-  }
-`
+`;
 
 const DevsInfo = styled.div`
-  width : 100%;
+  width: 100%;
   padding: 24px 24px 8px 24px;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 const DevSocial = styled.div`
-  padding : 8px 24px 24px 24px;
-  border-top : 1px solid rgba(0,0,0,0.1);
-`
-
+  padding: 16px 24px 16px 24px;
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
+  display : flex;
+  align-items : center;
+  justify-content : center;
+  gap: 8px;
+`;
 
 const SystemDevsComponent: React.FC<SystemDevType> = ({
-  pos, name, github, mail
+  pos,
+  name,
+  github,
+  mail,
 }) => {
   return (
     <DevsItem>
       <DevsInfo>
-        <ImgComponent 
+        <ImgComponent
           src="/images/img_user_default.jpg"
           objectFit="cover"
           width="64px"
@@ -64,7 +70,9 @@ const SystemDevsComponent: React.FC<SystemDevType> = ({
         </TypoComponent>
       </DevsInfo>
       <DevSocial>
-      
+        <Icon iconShape={faGithub} iconWidth="22px" iconHeight="22px" />
+        <Icon iconShape={faInstagramSquare} iconWidth="22px" iconHeight="22px" />
+        <Icon iconShape={faEnvelope} iconWidth="22px" iconHeight="22px" />
       </DevSocial>
     </DevsItem>
   );
