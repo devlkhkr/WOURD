b0fa34ea3a93789b71ebee173f273d0028add7cb
LeeKangHyoung <84236655+devlkhkr@users.noreply.github.com>
2022-11-21 17:46:30 +0900

    [Modify] 단어장 조회시 카테고리 플래그 함꼐 조회

--- pages/api/myword/list.ts
+++ pages/api/myword/list.ts
@@ -16,7 +16,10 @@ export default function getMyWordlist(
       "word_desc," +
       "word_state," +
       "state_modified_date," +
-      "word_reg_userid" +
+      "word_reg_userid," +
+      "word_is_cs_flag," +
+      "word_is_web_flag," +
+      "word_is_native_flag" +
       " FROM USER_WORD_TB LEFT OUTER JOIN WORD_TB ON USER_WORD_TB.word_id = WORD_TB.word_id WHERE user_id='" +
       req.body.params.userId +
       "' AND NOT word_state='s' AND word_use_flag=1 ORDER BY state_modified_date desc",
