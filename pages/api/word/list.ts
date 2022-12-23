af292e728853d613c60f371f23d70836f5f4ccfa
LeeKangHyoung <84236655+devlkhkr@users.noreply.github.com>
2022-12-23 18:07:32 +0900

    [Modify] 메인단어 호출 API에서 세션 호출

--- pages/api/word/list.ts
+++ pages/api/word/list.ts
@@ -2,8 +2,21 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 const db = require("../../../common/config/db");
 
-export default function getWordlist(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.query.userId);
+import { authOptions } from "pages/api/auth/[...nextauth]";
+import { unstable_getServerSession } from "next-auth/next";
+
+export default async function getWordlist(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const session = await unstable_getServerSession(req, res, authOptions);
+  if (!session) {
+    res.status(401).json({ message: "You must be logged in." });
+    return;
+  }
+
+  console.log(session);
+
   db.query("SELECT * FROM WORD_TB", function (err: any, result: any) {
     if (err) {
       console.log(err);
