5b2a49b40508bcdee40dd6d7136d488b8ef04e0f
LeeKangHyoung <84236655+devlkhkr@users.noreply.github.com>
2022-12-27 18:38:55 +0900

    [Feat] Snow Alert 구현

--- pages/api/word/list.ts
+++ pages/api/word/list.ts
@@ -4,6 +4,21 @@ const db = require("../../../common/config/db");
 
 import { authOptions } from "pages/api/auth/[...nextauth]";
 import { unstable_getServerSession } from "next-auth/next";
+import { mainWordExpOpts } from "next-auth";
+
+const getMainWordExpQuery = (
+  mainWordExpOpts: mainWordExpOpts | null | undefined
+) => {
+  const query =
+    "SELECT " +
+    "user_id," +
+    "user_password," +
+    "log_date " +
+    "FROM USER_TB, LOG_HISTORY_TB WHERE user_id='" +
+    "req.body.loginUserData.id" +
+    "' && USER_TB.user_id = LOG_HISTORY_TB.log_user_id && log_action=1 ORDER BY log_date DESC LIMIT 1;";
+  return "";
+};
 
 export default async function getWordlist(
   req: NextApiRequest,
@@ -14,7 +29,9 @@ export default async function getWordlist(
     res.status(401).json({ message: "You must be logged in." });
     return;
   }
-  console.log(session.user);
+  console.log(session.user.email);
+  console.log(session.user.mainWordExpOpts);
+  getMainWordExpQuery(session.user.mainWordExpOpts);
   db.query("SELECT * FROM WORD_TB", function (err: any, result: any) {
     if (err) {
       console.log(err);
