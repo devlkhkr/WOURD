0c2396cdb3a1d3f63cc58ff35e1253e402c2b960
bibiboy <95161113+chromeheartz@users.noreply.github.com>
2022-12-29 23:06:18 +0900

    [Modify] Spec 부분 데이터, 타입 구현중
    - Object.entries로 배열로 돌려 map 함수 호출
    - package.json import해서 각 모듈 버젼 정보 가져오기

--- pages/components/organisms/SystemSpec.tsx
+++ pages/components/organisms/SystemSpec.tsx
@@ -2,34 +2,33 @@ import axios from "axios";
 import { useState } from "react";
 import styled, { StyledInterface } from "styled-components";
 import TypoComponent from "../atoms/Typo";
+import StackComponent from "../molecules/StackComponent";
 import SystemDevsComponent from "../molecules/SystemDevs";
 
-interface SystemSpecTypes extends StyledInterface {
+interface SystemSpecTypes extends StyledInterface {}
 
-}
-
-const SystemDevsWrap = styled.ul`
-  display : flex;
+const StackWrap = styled.ul`
+  display: flex;
   flex-direction: column;
-  align-items : center;
-  gap : 16px;
-  padding : 0 4px;
-  margin-top : 16px;
-`
+  align-items: center;
+  gap: 16px;
+  padding: 0 4px;
+  margin-top: 16px;
+`;
 
-const stackWrap = styled.li`
-`
+// const StackComponent = styled.li`
+
+// `
 
 const SystemSpec: React.FC = () => {
-
-  const [ devs, setDevs ] = useState([
+  const [devs, setDevs] = useState([
     {
       id: 1,
       pos: "Full-Stack Dev",
       name: "Kany Kang",
       github: "devlkhkr",
       mail: "devlkhkr@gmail.com",
-      instagram: "cnpea"
+      instagram: "cnpea",
     },
     {
       id: 2,
@@ -37,28 +36,53 @@ const SystemSpec: React.FC = () => {
       name: "Amazing2bobii",
       github: "chromeheartz",
       mail: "barnesquiat@gmail.com",
-      instagram: "barnesquiat"
-    }
-  ])
+      instagram: "barnesquiat",
+    },
+  ]);
+
+  // Object.entries()
+  // 배열로 뿌려줌.
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+
+  // package.json 버전정보 가져와서 뿌려주게 해보기?
+  const objSpecs = {
+    specification: {
+      nextjs: {
+        name: "next-js",
+        src: "spec-nextjs.png",
+        by: "Vercel",
+        docs: "https://nextjs.org/",
+        desc: "The React Framework for Production",
+      },
+      react: {
+        name: "react",
+        src: "",
+        by: "",
+        docs: "",
+        desc: "",
+      }
+    },
+  };
 
   const getApi = async () => {
     const res = await axios.get("http://localhost:3000" + "/api/word/list");
-    console.log(res)
-  }
+    console.log(res);
+  };
 
   return (
     <>
-      <TypoComponent
-        fontSize="18px"
-        fontWeight="bold"
-        textAlign="left"
-        color="#202020"
-      >
-        Developers
-      </TypoComponent>
-      <SystemDevsWrap>
-        {
-          devs.map(dev => (
+      {/* developers */}
+      <section className="developers-section">
+        <TypoComponent
+          fontSize="18px"
+          fontWeight="bold"
+          textAlign="left"
+          color="#202020"
+        >
+          Developers
+        </TypoComponent>
+        <StackWrap>
+          {devs.map(dev => (
             <SystemDevsComponent
               key={dev.id}
               pos={dev.pos}
@@ -67,31 +91,32 @@ const SystemSpec: React.FC = () => {
               mail={dev.mail}
               instagram={dev.instagram}
             />
-          ))
-        }
-      </SystemDevsWrap>
-      <TypoComponent
-        fontSize="18px"
-        fontWeight="bold"
-        textAlign="left"
-        color="#202020"
-        marginTop="16px"
-        marginBottom="16px"
-      >
-        
-        Specification
-      </TypoComponent>
-      <div>
-        
-      </div>
-      <TypoComponent
-        fontSize="18px"
-        fontWeight="bold"
-        textAlign="left"
-        color="#202020"
-      >
-        Modules
-      </TypoComponent>
+          ))}
+        </StackWrap>
+      </section>
+
+      {/* specification */}
+      <section className="specification-section">
+        <TypoComponent
+          fontSize="18px"
+          fontWeight="bold"
+          textAlign="left"
+          color="#202020"
+          marginTop="16px"
+          marginBottom="16px"
+        >
+          Specification
+        </TypoComponent>
+        <StackWrap>
+          <StackComponent
+            name="next-js"
+            src="spec-nextjs.png"
+            by="Vercel"
+            docs="https://nextjs.org/"
+            desc="The React Framework for Production"
+          />
+        </StackWrap>
+      </section>
     </>
   );
 };
