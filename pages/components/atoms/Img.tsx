2e338bfebf09fb7b292862f0b17ff99d5370b66b
LeeKangHyoung <84236655+devlkhkr@users.noreply.github.com>
2023-01-03 18:19:07 +0900

    [Style] 이미지 컴포넌트 prettier

--- pages/components/atoms/Img.tsx
+++ pages/components/atoms/Img.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import styledInterface from "../Intefaces/styledComponent"
-interface ImgTypes extends styledInterface{
+import styledInterface from "../Intefaces/styledComponent";
+interface ImgTypes extends styledInterface {
   src?: string;
   alt?: string;
   objectFit?: string;
@@ -10,19 +10,35 @@ interface ImgTypes extends styledInterface{
 
 const ImgStyled = styled.img<ImgTypes>`
   display: inline-block;
-  width: ${(props => props.width)};
-  height: ${(props => props.height)};
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
   border-radius: 50%;
-  border: ${(props) => props.borderColor ? (`1px solid ${props.borderColor}`) : ( '1px solid var(--color-deepgrey)' )};
+  border: ${(props) =>
+    props.borderColor
+      ? `1px solid ${props.borderColor}`
+      : "1px solid var(--color-deepgrey)"};
   object-fit: ${(props) => props.objectFit || "unset"};
-  margin-bottom : ${(props) => props.marginBottom || "0"};
+  margin-bottom: ${(props) => props.marginBottom || "0"};
 `;
-const ImgComponent: React.FC<ImgTypes>  = ({
-  src, objectFit, marginBottom, width, height, borderColor
+const ImgComponent: React.FC<ImgTypes> = ({
+  src,
+  objectFit,
+  marginBottom,
+  width,
+  height,
+  borderColor,
 }) => {
-  return(
-    <ImgStyled src={src} alt="user-image" width={width} height={height} objectFit={objectFit} marginBottom={marginBottom} borderColor={borderColor} />
-  )
+  return (
+    <ImgStyled
+      src={src}
+      alt="user-image"
+      width={width}
+      height={height}
+      objectFit={objectFit}
+      marginBottom={marginBottom}
+      borderColor={borderColor}
+    />
+  );
 };
 
 ImgComponent.defaultProps = {};
