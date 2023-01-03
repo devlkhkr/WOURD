a1b4d9578efa13038d14e9fd12ec24850ea52f5d
LeeKangHyoung <84236655+devlkhkr@users.noreply.github.com>
2023-01-03 17:49:24 +0900

    [Modify] 헤더 컴포넌트 path 별 조건부 렌더링

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBolt } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowLeft,
+  faArrowRightFromBracket,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
 
 import styled from "styled-components";
 import Logo from "../components/atoms/Logo";
+import { useRouter } from "next/router";
+import Icon from "./atoms/Icon";
+import { signOut } from "next-auth/react";
 
-const HeaderWrap = styled.header`
+interface HeaderComponentTypes {}
+
+const HeaderWrapStyled = styled.header<HeaderComponentTypes>`
   background-color: blue;
   width: 100%;
   height: var(--height-header);
@@ -14,24 +23,121 @@ const HeaderWrap = styled.header`
   position: relative;
   display: flex;
   justify-content: center;
+  box-shadow: 0px 0px 8px 2px rgba(0, 0, 0, 0.05);
 `;
 
 const HeaderLogo = styled.div`
   display: flex;
   align-items: center;
+  h2 {
+    font-size: 16px;
+    font-weight: var(--weight-bold);
+  }
 `;
 
-const Header: React.FC = () => {
+const topIconPosition = `
+  position: absolute;
+  top: 50%;
+  transform: translate(0, -50%);
+`;
+
+const HistoryBack = styled.span`
+  ${topIconPosition}
+  left: 20px;
+`;
+
+const AddNewWord = styled.span`
+  ${topIconPosition}
+  right: 20px;
+`;
+
+const HeaderComponent: React.FC<HeaderComponentTypes> = ({}) => {
+  const router = useRouter();
+  const [canGoBack, setCanGoBack] = useState<boolean>();
+
+  useEffect(() => {
+    window.history.length > 1 ? setCanGoBack(true) : setCanGoBack(false);
+  });
+
+  const addNewWordClick = () => {
+    router.push("/MyWords/Regist");
+  };
+
+  const getHdrTitle = (path: string) => {
+    switch (path) {
+      case "/":
+        return <Logo mainColor="var(--color-point)" subColor="#231815" />;
+      case "/MyWords":
+        return <h2>단어장</h2>;
+      case "/MyWords/Regist":
+        return <h2>새로운 단어 등록</h2>;
+      case "/Setting":
+        return <h2>설정</h2>;
+      case "/Setting/Profile":
+        return <h2>프로필 변경</h2>;
+      default:
+        return <></>;
+    }
+  };
+
+  const getHdrRightIcon = (path: string) => {
+    switch (path) {
+      case "/Setting":
+        return (
+          <Icon
+            iconShape={faArrowRightFromBracket}
+            iconWidth="20px"
+            iconHeight="20px"
+            align="auto"
+            color="var(--color-grey)"
+            onClick={() =>
+              signOut({
+                redirect: true,
+                callbackUrl: "/Login",
+              })
+            }
+          />
+        );
+      case "/":
+      case "/MyWords":
+        return (
+          <Icon
+            iconShape={faPlus}
+            iconWidth="20px"
+            iconHeight="20px"
+            align="auto"
+            color="var(--color-grey)"
+            onClick={addNewWordClick}
+          />
+        );
+      default:
+        return <></>;
+    }
+  };
+
+  const addWordDeniedList = ["/Setting", "/MyWords/Regist"];
+
   return (
-    <HeaderWrap>
-      {/* <FontAwesomeIcon icon="fa-brands fa-discord" /> */}
-      <HeaderLogo>
-        {/* <FontAwesomeIcon icon={faBolt} />
-        <h1>DINDER</h1> */}
-        <Logo mainColor="var(--color-point)" subColor="#231815" />
-      </HeaderLogo>
-    </HeaderWrap>
+    <HeaderWrapStyled>
+      {canGoBack ? (
+        <HistoryBack>
+          <Icon
+            iconShape={faArrowLeft}
+            iconWidth="20px"
+            iconHeight="20px"
+            align="auto"
+            color="var(--color-grey)"
+            onClick={router.back}
+          />
+        </HistoryBack>
+      ) : (
+        <></>
+      )}
+      <HeaderLogo>{getHdrTitle(router.pathname)}</HeaderLogo>
+
+      <AddNewWord>{getHdrRightIcon(router.pathname)}</AddNewWord>
+    </HeaderWrapStyled>
   );
 };
 
-export default Header;
+export default HeaderComponent;
