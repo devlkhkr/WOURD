405c4b687f1af9969b926697613bdf98c0e4f547
LeeKangHyoung <84236655+devlkhkr@users.noreply.github.com>
2023-02-13 05:22:07 +0900

    [Fix] Issue#44 Too many connections 오류 수정 테스트 1차

--- common/config/db.ts
+++ common/config/db.ts
@@ -1,18 +1,28 @@
 export {};
-const mysql = require("mysql");
-let db;
+const mysql = require('mysql');
 
-try {
-  db = mysql.createConnection({
-    user: process.env.NEXT_PUBLIC_DB_USER,
-    password: process.env.NEXT_PUBLIC_DB_PW,
-    host: process.env.NEXT_PUBLIC_DB_HOST,
-    port: process.env.NEXT_PUBLIC_DB_PORT,
-    database: process.env.NEXT_PUBLIC_DB_DATABASE,
-    charset: process.env.NEXT_PUBLIC_DB_CHARSET,
-  });
-} catch (err) {
-  console.error(err);
+let pool = mysql.createPool({
+  user: process.env.NEXT_PUBLIC_DB_USER,
+  password: process.env.NEXT_PUBLIC_DB_PW,
+  host: process.env.NEXT_PUBLIC_DB_HOST,
+  port: process.env.NEXT_PUBLIC_DB_PORT,
+  database: process.env.NEXT_PUBLIC_DB_DATABASE,
+  charset: process.env.NEXT_PUBLIC_DB_CHARSET,
+});
+
+const db = {
+  query : (query:string, callback:Function) => {
+    db.getConnection((conn:any)=>{
+      conn.query(query, callback)
+      conn.release();
+    })
+  },
+  getConnection : (callback:Function)=>{
+    pool.getConnection((err:Error, conn:any)=>{ 
+      if(err)throw err;
+      callback(conn);
+    });
+  }
 }
 
-module.exports = db;
+module.exports = db; //db객체 반환
