4dcb8187b3f6d82c096b26aed99fe69fb9a5c4b3
bibiboy <barnesquiat@gmail.com>
2023-02-15 16:55:43 +0900

    [Style] dangerousinnerHTML 아코디언 컴포넌트 정리

--- pages/components/molecules/AccordionBoardArticle.tsx
+++ pages/components/molecules/AccordionBoardArticle.tsx
@@ -14,20 +14,39 @@ const AccordionBoardArticleComponent = styled.div<AccordionBoardArticleTypes>`
   max-height: ${props => (props.isOpened ? "60vw" : "0")};
 `;
 
+interface ArticleProps {
+  article: any;
+}
+const NoticeArticle = styled.p<ArticleProps>`
+  font-size: 14px;
+  font-weight: var(--weight-regular);
+  line-height: 1.2;
+  color: inherit;
+  text-align: left;
+  margin-top: 4px;
+  margin-bottom: unset;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  word-break: break-word;
+  display: -webkit-box;
+`
+
 const AccordionBoardArticle: React.FC<AccordionBoardArticleTypes> = ({
   article,
   isOpened,
 }) => {
+  console.log(article)
   return (
     <AccordionBoardArticleComponent isOpened={isOpened} article={article}>
-      <TypoComponent
+      {/* <TypoComponent
         fontSize="14px"
         fontWeight="regular"
         textAlign="left"
         marginTop="4px"
       >
         {article}
-      </TypoComponent>
+      </TypoComponent> */}
+      <NoticeArticle article={article} dangerouslySetInnerHTML={{__html: article}}></NoticeArticle>
     </AccordionBoardArticleComponent>
   );
 };
