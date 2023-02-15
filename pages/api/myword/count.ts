9ade31a0937c81d9c62a7c7cea14681c33f1d62b
LeeKangHyoung <84236655+devlkhkr@users.noreply.github.com>
2023-02-15 17:57:50 +0900

    [Style] 유저 단어 count api prettier

--- pages/api/myword/count.ts
+++ pages/api/myword/count.ts
@@ -4,8 +4,8 @@ import { authOptions } from "../auth/[...nextauth]";
 const db = require("common/config/db");
 
 const getSttCntQuery = (userId: string) => {
-  return `SELECT word_state, COUNT(word_state) AS state_count FROM COPUBDB.USER_WORD_TB WHERE user_id="${userId}" GROUP BY word_state UNION SELECT "m" as word_state, COUNT(word_reg_userid) AS word_reg_count FROM COPUBDB.WORD_TB WHERE word_reg_userid="${userId}"`
-}
+  return `SELECT word_state, COUNT(word_state) AS state_count FROM COPUBDB.USER_WORD_TB WHERE user_id="${userId}" GROUP BY word_state UNION SELECT "m" as word_state, COUNT(word_reg_userid) AS word_reg_count FROM COPUBDB.WORD_TB WHERE word_reg_userid="${userId}"`;
+};
 
 export default async function getStatesCount(
   req: NextApiRequest,
@@ -16,11 +16,11 @@ export default async function getStatesCount(
     res.status(401).json({ message: "You must be logged in." });
     return;
   }
-  const ReqFullQuery:string = getSttCntQuery(session.user.email!)
+  const ReqFullQuery: string = getSttCntQuery(session.user.email!);
   db.query(ReqFullQuery, function (err: any, result: any) {
     if (err) {
       console.log(err);
-      res.send(err)
+      res.send(err);
     } else {
       res.send(result);
     }
