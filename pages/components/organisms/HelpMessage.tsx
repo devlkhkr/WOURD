a320f444dc44dc00d7cf9a1349326ca200e4f9ae
bibiboy <barnesquiat@gmail.com>
2023-02-15 17:07:15 +0900

    [Style] faq 아코디언 틀 정리

--- pages/components/organisms/HelpMessage.tsx
+++ pages/components/organisms/HelpMessage.tsx
@@ -13,18 +13,21 @@ const HelpMessage: React.FC = () => {
   const [helpAcrdData, setHelpAcrdData] = useState<acrdDataTypes[]>([
     {
       id: 0,
-      title: "당신이 알고싶은 무언가에 대한 제목",
-      date: "11.1 NOV",
+      title: "Wourd는 무료인가요?",
+      date: "2.15 Feb",
       article:
-        "이것을 알고싶다면 조금만 기다려주세요. 곰방 우리가 여기다가 업데이트 해줄랑께 알겠져?",
+        `https://wourd.com에 접속하여 웹 버전으로 이용하실 수 있습니다. <br>
+        기본적으로 프로필을 만들고, 단어에 대한 상태를 방향으로 카드를 넘기면서 관리할 수 있는 무료 기능이 제공됩니다.`,
       isOpened: false,
     },
     {
       id: 1,
-      title: "미란이노래 좋다",
-      date: "10.29 OCt",
+      title: "Wourd는 어떤 서비스인가요?",
+      date: "2.15 Feb",
       article:
-        "미란이는 왜이렇게 매력이 있을까요? 비비랑 미란이랑 둘중에 누가 이쁜걸까 나는 비비에 한표를 주겠다",
+        `CS에서 사용되는 단어들을 카테고리별 / 상태별로 저장하여 관리할 수 있습니다. <br>
+        다른 사용자가 등록한 단어들을 살펴보고 내가 생성한 단어를 공유하여 모두가 지식의 문을 <br>
+        넓혀나가는 것이 서비스의 목표입니다.`,
       isOpened: false,
     },
   ]);
@@ -37,7 +40,7 @@ const HelpMessage: React.FC = () => {
   return (
     <>
       <ul>
-        {helpAcrdData?.map((data, index) => {
+        {helpAcrdData?.reverse().map((data, index) => {
           return <AccordionBoardList key={data.id} acrdData={data} />;
         })}
       </ul>
