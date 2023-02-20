6abc1c3e285c281d6e87b7e534cdd30752e9a02c
LeeKangHyoung <84236655+devlkhkr@users.noreply.github.com>
2023-02-21 07:43:20 +0900

    [Refactor] typo component display 세팅 변경

--- pages/components/atoms/Typo.tsx
+++ pages/components/atoms/Typo.tsx
@@ -9,7 +9,9 @@ interface TypoType extends styledInterface {
 
 const TypoStyled = styled.div<TypoType>`
   display: ${(props) =>
-    props.textAlign === "center" ? props.display : "block" || "-webkit-box"};
+    props.textAlign === "center"
+      ? props.display
+      : props.display || "-webkit-box"};
   font-size: ${(props) => props.fontSize || "14px"};
   font-weight: ${(props) => `var(--weight-${props.fontWeight || "regular"})`};
   line-height: ${(props) => props.lineHeight || "1.2"};
